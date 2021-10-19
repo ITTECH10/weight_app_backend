9213b26c2e7bb7b2130ad088b14ababd1cd7e663
ITTECH10 <ittech.a1development@gmail.com>
2021-10-19 13:27:04 +0200

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const userRouter = require('./routers/UserRouter')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/v1/users', userRouter)
 
